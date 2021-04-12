6baf6c6d3f0156bd9663fe7d3eebc16e88bc52ef
Akinyemi Kehinde <kennyjacob06@gmail.com>
2021-04-12 15:22:34 +0100

    p

--- picture.php
+++ picture.php
@@ -2,7 +2,7 @@
               <div class="container">
                 <h4 style="text-align: center;">PICTURE HIGHLIGHT</h4>
                 <div class="d-flex align-items-start bg-light mb-3">
-                  <div class="row" style="margin: 0px 20px;">
+                  <div class="row" style="margin-top: 20px;">
                     <div class="col-md-4 col-sm-4">
                       <div class="card" style="width: 23rem;">
                         <img src="img/s10.jpg" class="card-img-top" alt="...">
@@ -51,7 +51,7 @@
 
                 <div class="container">
                 <div class="d-flex align-items-start bg-light mb-3">
-                  <div class="row" style="margin: 20px 20px;">
+                  <div class="row" style="margin-top: 20px;">
                     <div class="col-md-4 col-sm-4">
                       <div class="card" style="width: 23rem;">
                         <img src="img/s6.jpg" class="card-img-top" alt="...">

1c1901f4f676f9ee852486c53e9379663177dea1
Akinyemi Kehinde <kennyjacob06@gmail.com>
2021-04-12 15:08:46 +0100

    boss

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+include("navbar.php");
+ ?>
+ <?php
+include ("silder.php");
+?>
+<?php  
+include("picture.php");
+?>
+
+<?php
+include("test.php");
+?>
+
+<?php
+include ("footer.php");
+?>
---
+++ silder.php
@@ -0,0 +1,42 @@
+<header>
+  <!-- Intro settings -->
+  <style>
+    /* Default height for small devices */
+    #intro-example {
+      height: 400px;
+    }
+
+    /* Height for devices larger than 992px */
+    @media (min-width: 992px) {
+      #intro-example {
+        height: 600px;
+      }
+    }
+  </style>
+
+  <!-- Background image -->
+  <div
+    id="intro-example"
+    class="p-5 text-center bg-image"
+    style="background-image: url('img/s7.jpg');"
+  >
+    <div class="mask" style="background-color: rgba(0, 0, 0, 0.7);">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+          <h1 class="mb-3">A DAY WITH THE BOSS</h1>
+          <h5 class="mb-4">With the Esteemed CEO</h5>
+          <a
+            class="btn btn-outline-light btn-lg m-2"
+            href="https://www.youtube.com/watch?v=c9B4TPnak1A"
+            role="button"
+            rel="nofollow"
+            target="_blank"
+            >Click here
+            </a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Background image -->
+</header>
